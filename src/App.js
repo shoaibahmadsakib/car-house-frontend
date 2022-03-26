ca04e554249a557bdb9c846a803638bb6a8022f2
shoaibahmadsakib <shoaibahmadsakib@gmail.com>
2022-03-26 14:20:58 +0600

    write a question

--- src/App.js
+++ src/App.js
@@ -7,7 +7,23 @@ function App() {
     <div className="App">
       <Header></Header>
       <Shop></Shop>
+      <div className="para_style">
+      <h1>Question and answer</h1>
+      <b>How react work?</b>
+      <p>React is a JavaScript Libarary for building user interfaces
+
+        React has a virtual Dome. It has a one Root directory.
+        It has a JSX element. It can use Props by parents to children, and It work just like a block element ,which is component
+        and a bable complier can create jsx to pure JavaScript.
+        
+      </p>
+      <b>How UseState work?</b>
+      <p>Use state is a Hook. Here Have a initial state and a update value state. Behind the seen It's look like a array, and a function.
+        Here You can create a value and Update by using "useState"
+      </p>
+      </div>
     
+
     </div>
   );
 }
